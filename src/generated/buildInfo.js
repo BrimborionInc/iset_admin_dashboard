49896429ba5c62c7c964440b8ffeaa5af4b5460e
BrimborionInc <bill@sillery.co.uk>
2026-07-12 09:14:04 -0400

    Stamp engineering audit TEST build metadata

--- src/generated/buildInfo.js
+++ src/generated/buildInfo.js
@@ -1,15 +1,15 @@
 const buildInfo = {
   "packageVersion": "0.1.0",
-  "releaseId": "20260710-conflict-disposition-notes-prod",
-  "buildTarget": "production",
-  "builtAt": "2026-07-10T10:44:12.544Z",
-  "gitCommit": "13ee8be37e9208c6c6de1ca8355db62313a160db",
-  "gitShort": "13ee8be3",
-  "gitDirty": false,
-  "publicReleaseLabel": "Release 20260710-conflict-disposition-notes-prod",
-  "publicReleaseDateEn": "10th July 2026",
-  "publicReleaseDateFr": "10 juillet 2026",
-  "displayLabel": "release 20260710-conflict-disposition-notes-prod | 13ee8be3"
+  "releaseId": "20260712-engineering-audit-test",
+  "buildTarget": "test",
+  "builtAt": "2026-07-12T13:14:04.912Z",
+  "gitCommit": "9637eadf3c2c292e9b6197b643fe89b1802ab708",
+  "gitShort": "9637eadf",
+  "gitDirty": true,
+  "publicReleaseLabel": "Release 20260712-engineering-audit-test",
+  "publicReleaseDateEn": "12th July 2026",
+  "publicReleaseDateFr": "12 juillet 2026",
+  "displayLabel": "release 20260712-engineering-audit-test | 9637eadf-dirty"
 };
 
 export default buildInfo;
