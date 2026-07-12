13260ee676b165e4dd62c66f106e0e3bd66fa1a2
BrimborionInc <bill@sillery.co.uk>
2026-07-12 09:14:12 -0400

    Normalize TEST build source stamp

--- src/generated/buildInfo.js
+++ src/generated/buildInfo.js
@@ -2,14 +2,14 @@ const buildInfo = {
   "packageVersion": "0.1.0",
   "releaseId": "20260712-engineering-audit-test",
   "buildTarget": "test",
-  "builtAt": "2026-07-12T13:14:04.912Z",
-  "gitCommit": "9637eadf3c2c292e9b6197b643fe89b1802ab708",
-  "gitShort": "9637eadf",
-  "gitDirty": true,
+  "builtAt": "2026-07-12T13:14:12.662Z",
+  "gitCommit": "49896429ba5c62c7c964440b8ffeaa5af4b5460e",
+  "gitShort": "49896429",
+  "gitDirty": false,
   "publicReleaseLabel": "Release 20260712-engineering-audit-test",
   "publicReleaseDateEn": "12th July 2026",
   "publicReleaseDateFr": "12 juillet 2026",
-  "displayLabel": "release 20260712-engineering-audit-test | 9637eadf-dirty"
+  "displayLabel": "release 20260712-engineering-audit-test | 49896429"
 };
 
 export default buildInfo;
